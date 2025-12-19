0ab39662cf4e48f69722a0a39807cf1b5bec7602
Aazeth <98008803+Aazeth@users.noreply.github.com>
2025-12-19 22:01:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
 function convert() {
-  const input = document.getElementById("linkInput").value
+  const input = document.getElementById("linkInput").value.trim()
+
+  if (!input) {
+    alert("pwease put a lwink fiwst")
+    return
+  }
 
   const match = input.match(/id=([^&]+)/)
   if (!match) {
-    alert("lwinknyaa ga vawlid")
+    alert("dis link is not vawlid")
     return
   }
 
   const fileId = match[1]
   const viewLink = `https://drive.google.com/file/d/${fileId}/view`
+
   window.location.href = viewLink
 }
 
 function pasteLink() {
-  navigator.clipboard.readText().then(text => {
-    document.getElementById("linkInput").value = text
-  })
+  if (!navigator.clipboard || !navigator.clipboard.readText) {
+    alert("clipboawd is not suppowted hewre")
+    return
+  }
+
+  navigator.clipboard.readText()
+    .then(text => {
+      if (!text) {
+        alert("clipboawd is empty")
+        return
+      }
+
+      document.getElementById("linkInput").value = text
+    })
+    .catch(() => {
+      alert("cannot paste fwom clipboawd. pwease allow pewmission")
+    })
 }
 
 function clearLink() {
