0eca8cb5a0c23d00af95b6cff41555a8c54aecbe
Aazeth <98008803+Aazeth@users.noreply.github.com>
2025-12-20 11:48:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,37 @@
+function toggleUwu() {
+  const box = document.getElementById("uwuBox")
+  box.style.display = box.style.display === "block" ? "none" : "block"
+}
+
+function clearAll() {
+  document.getElementById("linkInput").value = ""
+  document.getElementById("resultBox").classList.add("hidden")
+  document.getElementById("uwuBox").style.display = "none"
+}
+
 function convert() {
-  const input = document.getElementById("linkInput").value.trim()
-
-  if (!input) {
-    alert("pwease put a lwink fiwst")
-    return
-  }
-
+  const input = document.getElementById("linkInput").value
   const match = input.match(/id=([^&]+)/)
+
   if (!match) {
-    alert("dis link is not vawlid")
+    alert("lwink invalid uwu")
     return
   }
 
   const fileId = match[1]
   const viewLink = `https://drive.google.com/file/d/${fileId}/view`
 
-  window.location.href = viewLink
+  document.getElementById("resultLink").value = viewLink
+  document.getElementById("resultBox").classList.remove("hidden")
 }
 
-function pasteLink() {
-  if (!navigator.clipboard || !navigator.clipboard.readText) {
-    alert("clipboawd is not suppowted hewre")
-    return
-  }
-
-  navigator.clipboard.readText()
-    .then(text => {
-      if (!text) {
-        alert("clipboawd is empty")
-        return
-      }
-
-      document.getElementById("linkInput").value = text
-    })
-    .catch(() => {
-      alert("cannot paste fwom clipboawd. pwease allow pewmission")
-    })
+function copyLink() {
+  const text = document.getElementById("resultLink")
+  text.select()
+  document.execCommand("copy")
 }
 
-function clearLink() {
-  document.getElementById("linkInput").value = ""
-}
-function toggleUwu() {
-  const box = document.getElementById("uwuBox")
-  box.style.display = box.style.display === "block" ? "none" : "block"
+function openLink() {
+  const link = document.getElementById("resultLink").value
+  window.open(link, "_blank")
 }
